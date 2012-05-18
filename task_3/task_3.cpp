56a6209f3d0f97417435b31072dab0bd56976c69
Eugene Magergut <songshit@yandex.ru>
2012-05-18 05:46:42 +0400

    fixed

--- task_3/task_3.cpp
+++ task_3/task_3.cpp
@@ -2,36 +2,34 @@
 #include <vector>
 #include <assert.h>
 #include <math.h>
+#include <algorithm>
 using namespace std;
 
-
 template <class T>
-T Gcd(T& a, T& b){
+T Gcd(const T& a, const T& b){
 	T temp;
-	while (b != 0){
-		temp = b;
-		b = a % b;
-		a = temp;
+	T a2 = a;
+	T b2 = b;
+	while (b2 != 0){
+		temp = b2;
+		b2 = a2 % b2;
+		a2 = temp;
 	}
-	return a;
+	return a2;
 }
 
 template <class T>
-class polynom{
+class polynom
+{
 private:
 	int size;
 	vector <T> coef;
 public:
-	polynom(){
-		size = 0;
-		coef.resize(1, 1);
-	}
+	polynom(): size(0), coef(1, 1) {}
 
-	polynom(int size_){
-		size = size_;
-		coef.resize(size + 1);
-	}
-	polynom(polynom& b){
+	polynom(int size_): size(size_), coef(size_ + 1) {}
+
+	polynom(const polynom<T>& b){
 		size = b.GetSize();
 		coef.resize(size+1);
 		for (int i = 0; i <= size; i++){
@@ -39,43 +37,21 @@ public:
 		}
 	}
 
-	void CreatePolynom(int j){
-		srand(j);
-		for (int i = 0; i <= size; i++){
-			coef[i] = (rand() % 20);
-		}
-		while(1){
-			if (coef[size] == 0)
-				coef[size] = rand() % 20;	
-			else break;
-		}
-
+	T GetCoef(int index) const{
+		return coef[index];
 	}
-
-	void PrintOnScreen(){
-		for (int i = size; i >= 0; i--){
-			cout << coef[i];
-			if (i != 0)
-				cout << "x^" << i << " + ";
-		}
-		cout << endl << endl;
-	}
-
-	T GetCoef(int i){
-		return coef[i];
-	}
-	int GetSize(){
+	int GetSize() const{
 		return size;
 	}
-	int SetSize(int value){
+	void SetSize(int value){
 		size = value;
 	}
 
-	void SetCoef(int i, T value){
-		coef[i] = value;
+	void SetCoef(int index, T value){
+		coef[index] = value;
 	}
 
-	polynom<T> operator =(polynom<T>& b){
+	polynom<T> operator =(const polynom<T>& b){
 		size = b.GetSize();
 		coef.resize(size + 1);
 		for (int i = 0; i <= size; i++){
@@ -90,14 +66,20 @@ public:
 		return false;
 	}
 
-	bool operator ==(polynom<T>& b){
+	bool operator ==(const polynom<T>& b){
 		if(size != b.size) return false;
 		else{
 			for (int i = size; i >=0; i--){
-				if ((coef[i] - b.coef[i]) > 0.000000000001) return false;
+				if ((coef[i] - b.coef[i]) > Pogreshnost())
+					return false;
 			}
 		}
 		return true;
+	}
+
+	T Pogreshnost(){
+		if(typeid(T).name() == typeid(double).name())
+			return 0.00001;
 	}
 
 	polynom<T> operator *(T a){
@@ -106,9 +88,9 @@ public:
 		}
 	}
 
-	polynom<T> operator *(polynom<T>& a){
+	polynom<T> operator *(const polynom<T>& a){
 		polynom<T> result(GetSize() + a.GetSize());
-		for (int  i = size; i >= 0; i--){
+		for (int i = size; i >= 0; i--){
 			for (int j = a.size; j >= 0; j--){
 				result.coef[i + j] += a.coef[j] * coef[i];
 			}
@@ -116,17 +98,16 @@ public:
 		return result;
 	}
 
-	polynom<T> operator %(polynom<T>& b){
-		double temp;
+	polynom<T> operator %(const polynom<T>& b){
+		T temp;
 		for (int i = size; i >= b.GetSize(); i--){
 				temp = coef[i] / b.GetCoef(b.GetSize());
 				for (int j = b.GetSize(); j >= 0; j--){
 					coef[i - (b.GetSize() - j)] = coef[i - (b.GetSize() - j)] - (temp * b.GetCoef(j));
 				}
 		}
-
 		int k = size;
-		while (k >= 0 && abs(coef[k]) <= 0.0000001){
+		while (k >= 0 && abs(coef[k]) <= Pogreshnost()){
 			coef.erase(--coef.end());
 			k--;
 		}
@@ -142,6 +123,16 @@ public:
 		return *this;
 	}
 };
+
+ostream& operator << (ostream& out, const polynom<double>& P){
+	for (int i = P.GetSize(); i >= 0; i--){
+		out << P.GetCoef(i);
+		if (i != 0)
+			out << "x^" << i << " + ";
+	}
+	out << endl << endl;
+	return  out;
+}
 
 int ReminderRecursive(int a, int b){
 	if (a - b >= b){
@@ -165,72 +156,113 @@ int IntergerGcd(int a, int b){
 	}
 }
 
+
+void createCoefs(polynom<double>& P){
+	int size = P.GetSize();
+	for (int i = 0; i <= size; i++){
+		P.SetCoef(i, (rand() % 20));
+	}
+	while(1){
+		if (P.GetCoef(size) == 0)
+			P.SetCoef(size, (rand() % 20));
+		else break;
+	}
+}
+
 void IntergerTest(){
-	int a, b;
+	int a, b, answer;
 	srand(5);
-	a = rand() % 1000;
-	b = rand() % 1000;
-	cout << "gdc(" << a << ", " << b << ") = ";
-	int answer = Gcd<int>(a, b);
-	cout << answer << endl;
-	assert(answer == IntergerGcd(a, b));
+	for (int i = 0; i < 100; i++){
+		a = rand() % 1000;
+		b = rand() % 1000;
+		answer = Gcd<int>(a, b);
+		assert(answer == IntergerGcd(a, b));
+	}
+	cout << "IntegerTest is done" << endl;
 }
 
 void PolynomExample(){
-	polynom<double> P(3);
-	polynom<double> Q(2);
+	polynom<double> P(rand()%10);
+	polynom<double> Q(rand()%10);
 	
-	P.CreatePolynom(4);
-	Q.CreatePolynom(6);
-	
-	cout << "p(x) = ";
-	P.PrintOnScreen();
-	cout << "q(x) = ";
-	Q.PrintOnScreen();
-	
+	createCoefs(P);
+	createCoefs(Q);
+
+	cout << "p(x) = " << P;
+	cout << "q(x) = " << Q;
+
 	polynom<double> A;
 	A = Gcd<polynom<double>> (P, Q);
-	cout << "gcd(p,q) = ";
-	A.PrintOnScreen();
+	cout << "gcd(p,q) = " << A;
 }
 
-void PolynomTest(){
-	polynom<double> CommonPolynom;
-	polynom<double> A(1);
+
+double generatingNumber(vector<double>& arrayOfNumber){
+	double number;
+	do{
+		number = rand()%12;
+	}while(arrayOfNumber.end() != find(arrayOfNumber.begin(), arrayOfNumber.end(), number));
+	return number;
+}
+
+void CurrentPolynomTest(){
+	polynom<double> A(1), CommonPolynom;
 	A.SetCoef(1, 1);
-	for (int i = 1; i <= 5; i++){
-		A.SetCoef(0, -i);
+	
+	int count;
+	int CommonPolynomSize = rand()%5;
+	double number;
+	vector<double> arrayOfNumber;
+	
+	for (int i = 1; i <= CommonPolynomSize; i++){
+		number = generatingNumber(arrayOfNumber);
+		arrayOfNumber.push_back(number);
+		A.SetCoef(0, -number);
 		CommonPolynom = CommonPolynom * A;
 	}
-	cout << "CommonPolynom(x) = ";
-	CommonPolynom.PrintOnScreen();
 
+	count = rand()%5;
 	polynom<double> B(CommonPolynom);
-	for(int i = 6; i <= 7; i++){
-		A.SetCoef(0, -i);
+	while(count > 0){
+		number = generatingNumber(arrayOfNumber);
+		arrayOfNumber.push_back(number);
+		A.SetCoef(0, -number);
 		B = B * A;
+		count--;
 	}
-	cout << "B(x) = CommonPolynom(x) * P(x) = ";
-	B.PrintOnScreen();
 
+	count = rand()%5;
 	polynom<double> C(CommonPolynom);
-	for(int i = 8; i <= 10; i++){
-		A.SetCoef(0, -i);
+	while(count > 0){
+		number = generatingNumber(arrayOfNumber);
+		arrayOfNumber.push_back(number);
+		A.SetCoef(0, -number);
 		C = C * A;
+		count--;
 	}
-	cout << "C(x) = CommonPolynom(x) * Q(x) = ";
-	C.PrintOnScreen();
-
-	cout << "gcd (B,C) = ";
-	Gcd<polynom<double>>(B, C).PrintOnScreen();
-	assert(Gcd<polynom<double>>(B, C) == CommonPolynom);
-	cout << "So, gcd(B,C) = CommonPolynom";
+	
+	polynom<double> resultPolynom(Gcd<polynom<double>>(B, C));
+	assert(resultPolynom == CommonPolynom);
 }
 
+
+void PolynomTest(){
+	for (int i = 0; i < 1000; i++){
+		CurrentPolynomTest();
+	}
+	cout << "PolinomTest is done" << endl;
+}
+
+
 int main(){
-	IntergerTest();
 	PolynomExample();
 	PolynomTest();
+	IntergerTest();
+	
+	
 
+
+	int l;
+	cin >> l;
 	return 0;
 }
