0222ebbf741f7e367fe9bc6cf035c1f2fdba4f1e
Eugene Magergut <songshit@yandex.ru>
2012-05-18 05:46:25 +0400

    fixed

--- task_1/preCycle.cpp
+++ task_1/preCycle.cpp
@@ -1,28 +1,60 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <assert.h>
 using namespace std;
 
+vector<int> operator ++(vector<int>& element, int){
+	for (size_t i = 0; i < element.size(); i++){
+		element[i] += 1;
+	}
+	return element;
+}
+
+vector<int> operator +(const vector<int>& firstElement, const vector<int>& secondElement){
+	vector<int> result(firstElement);
+	for (size_t i = 0; i < result.size(); i++){
+		result[i] = firstElement[i] + secondElement[i];
+	}
+	return result;
+}
+
+vector<int> operator %(const vector<int>& element, int mod){
+	vector<int> result(element);
+	for (size_t i = 0; i < element.size(); i++){
+		result[i] = element[i] % mod;
+	}
+	return element;
+}
+
+ostream& operator << (ostream& out, const vector<int>& element){
+	out << "(";
+	for (size_t i = 0; i < element.size(); i++){
+		out << element[i] << ",";
+	}
+	out << ")";
+	return out;
+}
+
 template <class T>
-class functor{
+class Transformation{
 private:
 	int mod;
 public:
-	functor(T value){
-		mod = value;
-	}
-	T operator ()(T& x){
-		return (x + x) % mod;
+	explicit Transformation(int value_): mod(value_) {}
 
+	T operator()(const T& x) const{
+		return (x + x) % mod;
 	}
-	int GetMod(){
+
+	int getMod() const{
 		return mod;
 	}
 };
 
-template<class functor, class T>
-int PreCycleLength(functor& f, T x){
-	int n = f.GetMod();
+template<class Transformation, class T>
+int preCycleLengthSearch(const Transformation& f, T x){
+	int n = f.getMod();
 	T current = x;
 	for(int i = 0; i <= n; i++){
 		current = f(current);
@@ -33,7 +65,7 @@ int PreCycleLength(functor& f, T x){
 		element = f(element);
 		length++;
 	}
-	int prCycleLength = 0;
+	int preCycleLength = 0;
 	T x2 = x;
 	for(int i = 0; i < length; i++){
 		x2 = f(x2);
@@ -41,64 +73,89 @@ int PreCycleLength(functor& f, T x){
 	while (x != x2){
 		x = f(x);
 		x2 = f(x2);
-		prCycleLength++;
+		preCycleLength++;
 	}
-	return prCycleLength;
+	return preCycleLength;
 }
 
-
-class FunctorForTest{
+template <class T>
+class chainTransformation{
 private:
-	vector<int> f;
+	map<T, T> function_;
 	int mod;
 public:
-	FunctorForTest(int cycleLength, int preCycleLength){
-		f.resize(preCycleLength + cycleLength);
-		int currentElement = 0;
-		int nextElement = 0;
-		for (int i = 0; i < preCycleLength; i++){
+	chainTransformation(int cycleLength, int precycleLength, const T& firstElement): mod(precycleLength) {
+		T currentElement = firstElement;
+		T nextElement = firstElement;
+		for (int i = 0; i < precycleLength; i++){
 			nextElement++;
-			f[currentElement] = nextElement;
+			function_[currentElement] = nextElement;
 			currentElement = nextElement;
 		}
-		int repeatedElement = currentElement;
+		T repeatedElement = currentElement;
 		for (int i = 0; i < cycleLength - 1; i++){
 			nextElement++;
-			f[currentElement] = nextElement;
+			function_[currentElement] = nextElement;
 			currentElement = nextElement;
 		}
-		f[currentElement] = repeatedElement;
-		mod = preCycleLength;
+		function_[currentElement] = repeatedElement;
 	}
-	int GetMod(){
+	int getMod() const{
 		return mod;
 	}
-	int operator()(int value){
-		return f[value];
+	T operator() (const T& element) const{
+		return (function_.find(element))->second;
 	}
-};
+}; 
+
+
+void IntegerExample(){
+	int mod = rand();
+	int x = rand();
+	cout << "mod=" << mod << ", x=" << x << endl;
+	Transformation<int> f(mod);
+	cout << "PreCycle_length = " << preCycleLengthSearch(f, x) << endl;
+}
+
+void VectorExample(){
+	int mod = rand();
+	int vectorDimension = rand()%10;
+	vector<int> firstElement(vectorDimension);
+	for (int i = 0; i < vectorDimension; i++){
+		firstElement.push_back(rand());
+	}
+	cout << "mod=" << mod << ", x=" << firstElement << endl;
+	Transformation<vector<int>> f(mod);
+	cout << "PreCycle_length = " << preCycleLengthSearch(f, firstElement) << endl;
+}
 
 void TestForInteger(){
-	srand(3);
 	int cycleLength = rand() % 50;
 	int preCycleLength = rand() % 50;
-	FunctorForTest f(cycleLength, preCycleLength);
-	assert(PreCycleLength(f, 0) == preCycleLength);
-	cout << "Test is done";
+	chainTransformation<int> f(cycleLength, preCycleLength, 0);
+	assert(preCycleLengthSearch(f, 0) == preCycleLength);
+}
 
+void TestForVector(){
+	int vectorDimension = rand();
+	vector<int> firstElement(vectorDimension);
+	for (int i = 0; i < vectorDimension; i++){
+		firstElement.push_back(rand());
+	}
+
+	int cycleLength = rand() % 50;
+	int preCycleLength = rand() % 50;
+	chainTransformation<vector<int>> f(cycleLength, preCycleLength, firstElement);
+	assert(preCycleLengthSearch(f, firstElement) == preCycleLength);
 }
 
 int main(){
-	int mod;
-	cout << "Enter mod: ";
-	cin >> mod;
-	functor<int> f(mod);
-	int x;
-	cout << "Enter x: ";
-	cin >> x;
-	cout << "PreCycle_length = " << PreCycleLength<functor<int>, int>(f, x) << endl;
+	srand(5);
+	IntegerExample();
+	VectorExample();
 
 	TestForInteger();
+	TestForVector();
 
 	return 0;
 }
